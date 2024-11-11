046a0e32cd3a17bde2819511a06d186bf43ba9b3
Shocked2k23 <rantanenonni39@gmail.com>
2024-11-11 10:31:51 +0200

    workin

--- Projekti 1B/index.js
+++ Projekti 1B/index.js
@@ -1,18 +1,43 @@
 function lomaketarkistus(form) {
-  let name = form.nimisyotto.value;
-  let email = form.emailsyotto.value;
-  let age = form.ikasyotot.value;
-  let radio = form.radio.value;
-  let select = form.select.value;
+  let name = document.getElementById("nimisyotto").value;
+  let email = document.getElementById("emailsyotto").value;
+  let age = document.getElementById("ikasyotot").value;
+  let radio = document.querySelectorAll('input[name="radio"]');
+  let select = document.getElementById("pudotus").value;
 
   if (name < 3) {
     alert("Nimi on liian lyhyt");
-    form.nimisyotto.focus();
+    document.getElementById("nimisyotto").focus();
+    return false;
+  }
+
+  // Email RegEx
+  const isValidEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  // email validation after RegEx with isValidEmail()
+
+  if (email === "") {
+    alert("Sähköposti ei voi olla tyhjä!");
+    form.email.focus();
+    return false;
+  } else if (!isValidEmail(email)) {
+    alert("Anna kelvollinen sähköposti");
+    form.email.focus();
     return false;
   }
 
   if (age < 18) {
     alert("Lomake on vain 18 vuotta täyttäneille");
+    form.ikasyotot.focus();
+    return false;
+  }
+
+  if (age > 100) {
+    alert("Oletko varmasti yli 100 vuotias?");
     form.ikasyotot.focus();
     return false;
   }
@@ -40,25 +65,6 @@ function lomaketarkistus(form) {
 
   if (selectValue == false) {
     alert("Valitse Maa");
-    return false;
-  }
-
-  // Email RegEx
-  const isValidEmail = (email) => {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  };
-
-  // email validation after RegEx with isValidEmail()
-
-  if (email === "") {
-    alert("Sähköposti ei voi olla tyhjä!");
-    form.email.focus();
-    return false;
-  } else if (!isValidEmail(email)) {
-    alert("Anna kelvollinen sähköposti");
-    form.email.focus();
     return false;
   }
 }
